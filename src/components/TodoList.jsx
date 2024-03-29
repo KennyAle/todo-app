2705c51626be9ffbc314cc353f3cf7091ce676f0
KennyAle <kennyviver@gmail.com>
2024-03-29 13:23:28 -0700

    Creating add & delete todos Function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,37 @@
+import React, { useState } from 'react'
 function TodoList () {
+    const [todos, setTodos] = useState([])
+    const [input, setInput] = useState('')
+
+    function handleChange(e) {
+        setInput(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTodos([...todos, input])
+        setInput('')
+    }
+
+    function deleteTodo(index) {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
 
     return (
         <div>
+            <h1>Todo List</h1>
             <form>
-                <input type="text" />
-                <button>Add Todo</button>
+                <input type="text" value={input} onChange={handleChange} />
+                <button onClick={handleSubmit}>Add Todo</button>
             </form>
             <ul>
-                <li>
-                </li>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}
+                        <button onClick={() => deleteTodo(index)}>Delete</button>
+                    </li>
+                ))}
             </ul>
         </div>
             )
