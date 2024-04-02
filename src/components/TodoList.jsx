1dabec92471b798ef3eb20b66515d95ee7900338
KennyAle <kennyviver@gmail.com>
2024-04-02 13:03:53 -0700

    Styling Input, Buttons and Board

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-function TodoList () {
+function TodoList() {
     const [todos, setTodos] = useState([])
     const [input, setInput] = useState('')
 
@@ -20,23 +20,48 @@ function TodoList () {
     }
 
     return (
-        <div>
-            <h1>Todo List</h1>
-            <form>
-                <input type="text" value={input} onChange={handleChange} />
-                <button onClick={handleSubmit}>Add Todo</button>
-            </form>
-            <ul>
-                {todos.map((todo, index) => (
-                    <li key={index}>
-                        <input type="checkbox" id='task' name='task' />
-                        <label htmlFor='task'>{todo}</label>
-                        <button onClick={() => deleteTodo(index)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+        <div className='bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
+            <div class="p-5 mb-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <h1 class="text-lg font-semibold text-gray-900 dark:text-white">Todo List</h1>
+                <form>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5 12h14m-7 7V5" />
+                            </svg>
+                        </div>
+                        <input type="text" id="task" value={input} onChange={handleChange} class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buy groceries, Call mom, Pay bills..." />
+                        <button onClick={handleSubmit} type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Task</button>
+                    </div>
+                </form>
+                <ol class="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
+                    {todos.map((todo, index) => (
+                        <li class="flex justify-between p-3 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700" key={index}>
+                            <div>
+                                <p class="font-medium text-gray-900 dark:text-white">{todo}</p>
+                                <time class="text-sm font-normal">1:30 AM, 05/05/2024</time>
+                            </div>
+                            <div class="flex">
+                                <button onClick={() => deleteTodo(index)} type="button" class="text-blue-700 border border-blue-900 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-900 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 bg-blue-500">
+                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Delete button</span>
+                                </button>
+                                <button onClick={() => deleteTodo(index)} type="button" class="text-blue-700 border border-blue-900 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-900 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 bg-blue-500">
+                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd" />
+                                        <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Edit button</span>
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
+            </div>
         </div>
-            )
+    )
 }
 
 export default TodoList
