f63abd5391dd206c02d074e89dc9304a89079bb8
KennyAle <kennyviver@gmail.com>
2024-04-03 10:32:03 -0700

    Making Add Task button work only with the list's last item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -38,9 +38,10 @@ function TodoList() {
                                 <input type="text"
                                 id="task" 
                                 className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                placeholder="Enter New Task"
                                 value={task.task} 
                                 onChange={(e) => handleTaskEdit(e, index)} 
-                                onKeyDown={handleKeyDown}/>
+                                onKeyDown={index === tasksList.length -1 ? handleKeyDown : null}/>
                                 {index !== tasksList.length - 1 &&
                                     <button 
                                     onClick={() => handleTaskRemove(index)} 
