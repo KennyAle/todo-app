92962f3d994b2063bf50a5c291d584ac64f7194c
KennyAle <kennyviver@gmail.com>
2024-04-08 05:38:05 -0700

    Change task to green when checked

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TaskItem({ tasksList, task, index, inputRefs, handleTaskEdit, handleTaskRemove, handleBlur }) {
+    const [isChecked, setIsChecked] = useState(false)
+    
+    function handleTaskCheck() {
+        setIsChecked(!isChecked)
+    }
     return (
-        <div className='relative'>
+        <div className='relative w-full'>
             <input
                 ref={(el) => (inputRefs.current[index] = el)}
                 type="text"
                 id="task"
-                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                autoComplete="off"
+                className={`block w-full pl-3 pr-16 py-4 text-sm border rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 peer ${isChecked ? 'border-green-500 text-green-500 dark:bg-emerald-900 dark:border-green-500' : 'border-gray-300 text-gray-900 '}
+                `}
                 placeholder="Enter New Task"
                 value={task.task}
                 onChange={(e) => handleTaskEdit(e, index)}
-                onBlur={handleBlur}
-                /*onKeyDown={index === tasksList.length - 1 ? handleKeyDown: null}*/ />
+                onBlur={handleBlur} />
+            {/* <label class="peer-focus:font-medium absolute text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-1 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">5:23 AM, 01/06/2024</label> */}
+
             {index !== tasksList.length - 1 &&
-                <button
-                    onClick={() => handleTaskRemove(index)}
-                    type="button"
-                    className="text-slate-400 hover:text-slate-200 hover:transition-all absolute end-0.5 bottom-2.5   font-medium rounded-lg text-sm px-2 py-2">
-                    <svg className="opacity-60 w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                    </svg>
-                    <span className="sr-only">Delete button</span>
-                </button>
+                <div>
+                    <button
+                        onClick={() => handleTaskRemove(index)}
+                        type="button"
+                        className="text-slate-400 hover:text-red-400 hover:transition-all absolute end-0 bottom-2.5 font-medium rounded-lg text-sm px-1 py-2">
+                        <svg className="opacity-90 w-6 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.75" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                        </svg>
+                        <span className="sr-only">Delete button</span>
+                    </button>
+                    <button
+                        onClick={() => handleTaskCheck()}
+                        type="button"
+                        className={`hover:text-green-500 hover:transition-all absolute end-6 bottom-2.5 font-medium rounded-lg text-sm px-1.5 py-1
+                        ${isChecked ? 'text-green-500' : 'text-slate-400'}`}>
+                        <svg className="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 11.917 9.724 16.5 19 7.5" />
+                        </svg>
+                        <span className="sr-only">Mark as complete button</span>
+                    </button>
+                </div>
             }
         </div>
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,10 +9,6 @@ function TodoList({ listName, onListRemove }) {
         localStorage.setItem(`storedTasks_${listName}`, JSON.stringify(tasksList))
     }, [listName, tasksList])
 
-    // function handleTaskAdd() {
-    //     setTasksList([...tasksList, { task: "" }])
-    // }
-
     function handleTaskRemove(index) {
         const newTasks = [...tasksList]
         newTasks.splice(index, 1)
@@ -31,7 +27,7 @@ function TodoList({ listName, onListRemove }) {
             e.preventDefault()
             const index = tasksList.length - 1
             if (index === - 1 || tasksList[index].task.trim() !== '') {
-                const newTaskList = [...tasksList, { task: "" }]
+                const newTaskList = [...tasksList, { id: Date.now(), task: "" }]
                 setTasksList(newTaskList)
                 setTimeout(() => {
                     inputRefs.current[index + 1].focus()
@@ -43,7 +39,7 @@ function TodoList({ listName, onListRemove }) {
     function handleBlur() {
         const index = tasksList.length - 1
         if (index >= - 0 && tasksList[index].task.trim() !== '') {
-            const newTaskList = [...tasksList, { task: "" }]
+            const newTaskList = [...tasksList, { id: Date.now(), task: "" }]
             setTasksList(newTaskList)
         }
     }
@@ -54,26 +50,33 @@ function TodoList({ listName, onListRemove }) {
 
     return (
         <div className='relative rounded-md max-w-sm min-w-72 bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
+            <div className="w-full bg-gray-200 rounded-t-md h-2.5 dark:bg-gray-700">
+                <div className="bg-emerald-600 h-2.5 rounded-full w-[70%]" /*style="width: 45%"*/></div>
+            </div>
             <svg
                 onClick={handleListRemove}
-                className="absolute right-0 m-1 w-5 h-5 text-slate-400 hover:text-slate-200 hover:transition-all cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                className="absolute right-0 m-1 w-5 h-7 text-slate-400 hover:text-slate-200 hover:transition-all cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
             </svg>
             <span className='sr-only'>Delete list button</span>
-            <div className="flex flex-col gap-4 p-5 mb-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <svg className="absolute right-7 m-1 w-5 h-7 text-slate-400 hover:text-slate-200 hover:transition-all cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 11.5h13m-13 0V18a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-6.5m-13 0V9a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v2.5M9 5h11a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-1" />
+            </svg>
+            <div className="flex flex-col gap-4 p-5 mb-4 border border-gray-100 rounded-b-md bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <h2 className="text-sm text-slate-400">04/06/2024</h2>
                 <h1 className="text-lg font-semibold text-gray-900 dark:text-white">{listName}</h1>
                 <form onKeyDown={handleKeyDown} onBlur={handleBlur}>
                     <div className="relative flex flex-col gap-4">
                         {tasksList.map((task, index) => (
-                            <TaskItem 
-                            inputRefs={inputRefs}
-                            tasksList={tasksList}
-                            key={index} 
-                            index={index} 
-                            task={task} 
-                            handleTaskEdit={handleTaskEdit} 
-                            handleTaskRemove={handleTaskRemove} 
-                            handleBlur={handleBlur} />
+                            <TaskItem
+                                inputRefs={inputRefs}
+                                tasksList={tasksList}
+                                key={task.id}
+                                index={index}
+                                task={task}
+                                handleTaskEdit={handleTaskEdit}
+                                handleTaskRemove={handleTaskRemove}
+                                handleBlur={handleBlur} />
                         ))}
                     </div>
                 </form>
